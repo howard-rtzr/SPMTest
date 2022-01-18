2e17c4e8ab9d8775f52d604d8f62dd736203719e
howard <howard@rtzr.ai>
2022-01-18 11:12:23 +0900

    init spmTest.

---
+++ Sources/spmTest/spmTest.swift
@@ -0,0 +1,10 @@
+public struct spmTest {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+    
+    public func sayHello() -> String {
+        return text
+    }
+}
