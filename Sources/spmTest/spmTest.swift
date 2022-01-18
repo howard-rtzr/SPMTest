1d9af4e3514285af99416f7f0059c387a22b99d3
howard <howard@rtzr.ai>
2022-01-18 17:53:08 +0900

    add alamofire

--- Sources/spmTest/spmTest.swift
+++ Sources/spmTest/spmTest.swift
@@ -1,10 +1,14 @@
+import Alamofire
+
 public struct spmTest {
     public private(set) var text = "GCP"
 
     public init() {
     }
     
-    public func sayHello() -> String {
-        return text
+    public func sayHello(completion: @escaping (String) -> Void) -> Void {
+        AF.request("https://httpbin.org/get").response { response in
+            completion(response.description)
+        }
     }
 }
