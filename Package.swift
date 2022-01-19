0446f6084247fe092e7f3b8f89e050f1acf4816d
howard <howard@rtzr.ai>
2022-01-19 13:23:17 +0900

    Alamofire master

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(name: "Alamofire", url: "https://github.com/Alamofire/Alamofire", from: "4.3.0")
+        .package(name: "Alamofire", url: "https://github.com/Alamofire/Alamofire", branch: "master")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
